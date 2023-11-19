ec9010a8f33f2f38c403dd6c3eb4bd61a5c548cc
ade_lana <lanadvictor@yahoo.co.uk>
2023-11-19 14:23:13 +0100

    test: react testing with apollo client error reproduction

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
-import Counter from '../'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MockedProvider } from "@apollo/client/testing";
+import App, { TEST_MUTATION } from "../";
 
-test('increments the count', () => {
-  const {getByText} = render(<Counter />)
-  const button = getByText('0')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('1')
-  fireEvent.click(button)
-  expect(button).toHaveTextContent('2')
-})
+test("Should disable button", async () => {
+  const user = userEvent.setup();
+  const mocks = [
+    {
+      request: { variables: { name: "Test Name" }, query: TEST_MUTATION },
+      error: new Error("Network Error"),
+    },
+  ];
 
+  render(
+    <MockedProvider mocks={mocks}>
+      <App />
+    </MockedProvider>
+  );
+
+  await user.click(screen.getByRole("button", { name: "Click Me!" }));
+
+  expect(screen.getByRole("button", { name: "Click Me!" })).toBeDisabled();
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 
-function Counter() {
-  const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>{count}</button>
+export const TEST_MUTATION = gql`
+  mutation TestMutation($name: String!) {
+    testing(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+function App() {
+  const [status, setStatus] = useState("idle");
+  const [mutate] = useMutation(TEST_MUTATION);
+
+  const handleRequest = () => {
+    setStatus("loading");
+
+    mutate({
+      variables: { name: "Test Name" },
+      onError: () => setStatus("error"),
+    });
+  };
+
+  return (
+    <button onClick={handleRequest} disabled={status === "loading"}>
+      Click Me!
+    </button>
+  );
 }
 
-export default Counter
+export default App;
